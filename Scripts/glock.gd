1635f4b44a788d0de92323b4e31aae193400bc45
Agustin Poza <agustin.pozaai@gmail.com>
2025-08-13 15:31:55 -0300

    1

--- Scripts/glock.gd
+++ Scripts/glock.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+@onready var sight = $Glock/Area2D/Glock/Sight
+var dir
+
 func _physics_process(delta: float) -> void:
-	look_at(get_global_mouse_position())
+	var target_pos = get_global_mouse_position()
+	dir = target_pos.normalized()
+	look_at(target_pos)
+	if dir.x < 0:
+		$Glock/Area2D/Glock.flip_v = true
+		print(dir) 
+	else:
+		$Glock/Area2D/Glock.flip_v = false
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 @export var speed = 300
 @onready var animation_player = $AnimationPlayer
+@onready var gun = $Glock
 
 func _physics_process(delta: float) -> void:
 	
@@ -8,19 +9,32 @@ func _physics_process(delta: float) -> void:
 	velocity = direction*speed
 	
 	if(direction != Vector2.ZERO):
+		if (Input.is_action_just_pressed("shoot")):
+			moving_shoot()
 		animation_player.play("Walk")
 	else:
+		if (Input.is_action_just_pressed("shoot")):
+			shoot()
 		animation_player.play("Idle")
 	
 	move_and_slide()
-	if (Input.is_action_just_pressed("shoot")):
-		shoot()
 
 
 func shoot():
 	const projectile = preload("res://Scenes/bullet.tscn")
 	var bullet = projectile.instantiate()
-	bullet.global_position = global_position
+	bullet.global_position = gun.sight.global_position
 	bullet.look_at(get_global_mouse_position())
 	get_parent().add_child(bullet)
+	
+func moving_shoot():
+	const projectile = preload("res://Scenes/bullet.tscn")
+	var bullet = projectile.instantiate()
+	bullet.global_position = gun.sight.global_position
+	var original_vector = get_global_mouse_position().normalized()
+	var random_offset = Vector2(randf_range(-1,1),randf_range(-1,1))
+	var final_vector = original_vector * random_offset
+	bullet.look_at(final_vector)
+	get_parent().add_child(bullet)
+	
 	
